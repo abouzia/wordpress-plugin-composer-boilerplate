78e1af62c8ce46289fdceeb3ee29488de23acede
Yassine <yassine.abouzia@gmail.com>
2024-01-17 17:10:21 +0100

    refactor: optimaze code

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -8,6 +8,7 @@ class BaseController
     public $plugin_url;
     public $plugin_name;
     public $plugin;
+    public $managers;
 
     public function __construct()
     {
@@ -15,5 +16,17 @@ class BaseController
         $this->plugin_url = plugin_dir_url(dirname(__FILE__, 2));
         $this->plugin_name = plugin_basename($this->plugin_path);
         $this->plugin = $this->plugin_name . "/$this->plugin_name.php";
+
+        $this->managers = array(
+            'cpt_manager' => 'Activate CPT Manager',
+            'taxonomy_manager' => 'Activate Taxonomy Manager',
+            'media_widget' => 'Activate Media Widget',
+            'gallery_manager' => 'Activate Gallery Manager',
+            'testimonial_manager' => 'Activate Testimonial Manager',
+            'templates_manager' => 'Activate Templates Manager',
+            'login_manager' => 'Activate Login Manager',
+            'membership_manager' => 'Activate Membership Manager',
+            'chat_manager' => 'Activate Chat Manager'
+        );
     }
 }
